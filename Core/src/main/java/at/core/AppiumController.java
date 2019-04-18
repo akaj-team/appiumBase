488492c927dc7bc1ef19462aa2b79177c91ca77c
at-hangtran <hang.tran@asiantech.vn>
2019-04-18 16:11:53 +0700

    test

--- Core/src/main/java/at/core/AppiumController.java
+++ Core/src/main/java/at/core/AppiumController.java
@@ -47,10 +47,9 @@ public class AppiumController {
             capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, xmlTest.getParameter(AndroidMobileCapabilityType.APP_PACKAGE));
             capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, xmlTest.getParameter(AndroidMobileCapabilityType.APP_ACTIVITY));
 
-            File appDir = new File(classpathRoot, "/App/appfile/Android");
-            File appPath = new File(appDir, xmlTest.getParameter(MobileCapabilityType.APP));
-            //    capabilities.setCapability(MobileCapabilityType.APP, appPath.getAbsolutePath());
-            capabilities.setCapability(MobileCapabilityType.APP, xmlTest.getParameter(MobileCapabilityType.APP));
+//            File appDir = new File(classpathRoot, "//App/appfile/Android/");
+//            File appPath = new File(appDir, xmlTest.getParameter(MobileCapabilityType.APP));
+            capabilities.setCapability(MobileCapabilityType.APP, "//App/appfile/Android/" + xmlTest.getParameter(MobileCapabilityType.APP));
             driver = new AndroidDriver(new URL(xmlTest.getParameter("server")), capabilities);
         } else if (xmlTest.getParameter(MobileCapabilityType.PLATFORM_NAME).equalsIgnoreCase("ios")) {
             capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
